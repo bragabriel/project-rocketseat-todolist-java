4c5b5b853669689ec6b07e4cf10a11b0a06af5af
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2023-10-11 21:50:47 -0300

    feat: ORM and Repository

--- todolist/src/main/java/br/com/cursojava/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/cursojava/todolist/user/UserController.java
@@ -1,16 +1,32 @@
 package br.com.cursojava.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.micrometer.core.ipc.http.HttpSender.Response;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
     
+    @Autowired //gerencia, instancia, cuida do ciclo de vida do meu Repository
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
-        System.out.println(userModel.name);
+    public ResponseEntity create(@RequestBody UserModel userModel){
+        
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+        if(user != null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
