68fe923649c20688d34d2928977fd679e82ee54e
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2023-10-11 22:12:18 -0300

    feat: password encryption

--- todolist/src/main/java/br/com/cursojava/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/cursojava/todolist/user/UserController.java
@@ -2,13 +2,13 @@ package br.com.cursojava.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import io.micrometer.core.ipc.http.HttpSender.Response;
 
 @RestController
@@ -25,6 +25,10 @@ public class UserController {
         if(user != null){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
+
+        var passwordHashed = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashed);
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
